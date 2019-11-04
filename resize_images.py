c8e641123f16e53ec8af89fe6f8169c3a388c6f7
maknetaRo <makneta@gmail.com>
2019-11-04 22:03:08 +0100

    checking the proportion of the sides of the image

--- resize_images.py
+++ resize_images.py
@@ -1,3 +1,9 @@
+# python resize_images.py --input_dir=path/to/the/folder/with/pictures --output_dir=path/to/the/new/folder If folder is not specified the script will create the resized folder next to resize_image.py script.
+
+# DEFAULT size is width=1024 and height=768, if the width is shorter than height the width=768 and height=1024
+
+# but the size of resized images can be changed in command line python resize_image.py --width=320 --height=180
+
 import os 
 import argparse
 from PIL import Image 
@@ -13,10 +19,24 @@ def resize_image(input_dir, infile, output_dir='resized', size=DEFAULT_SIZE):
         img = Image.open(input_dir + '/' + infile)
         w,h = img.size
 
-        if w > h:        
-            img = img.resize((size[0], size[1]), Image.LANCZOS)
-        else:
-            img = img.resize((size[1], size[0]), Image.LANCZOS)
+        if w > h:
+            if int(h) / int(w) == 0.75 :        
+                img = img.resize((size[0], size[1]), Image.LANCZOS)
+            else: 
+                proportion = int(h) / int(w)
+                new_h = int(w) * proportion
+                DEFAULT_SIZE = (1024, new_h)
+                img = img.resize((size[0], size[1]), Image.LANCZOS)
+        elif w < h: 
+            if int(h) / int(w) ==0.75:
+                img = img.resize((size[1], size[0]), Image.LANCZOS)
+            else: 
+                proportion = int(h) / int(w)
+                new_w = int(h) * proportion
+                DEFAULT_SIZE = (new_w, 768)
+                img = img.resize((size[1], size[0]), Image.LANCZOS)
+        else: 
+            print("I don't know what to do!")
         new_file = output_dir + '/' + outfile + extension
         img.save(new_file)       
 
